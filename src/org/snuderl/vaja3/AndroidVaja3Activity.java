6fc21792e2b9f42bbd7117ca1e50d9db95a24e94
Snuderl <Snuderl@gmail.com>
2011-11-23 18:36:07 +0100

    Async task

--- src/org/snuderl/vaja3/AndroidVaja3Activity.java
+++ src/org/snuderl/vaja3/AndroidVaja3Activity.java
@@ -8,7 +8,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.snuderl.vaja3.RestClient.RequestMethod;
+import org.snuderl.vaja3.RestClient.Response;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,8 +22,8 @@ import android.widget.TextView;
 public class AndroidVaja3Activity extends Activity {
 
     /** Called when the activity is first created. */
-	String url ="http://free.worldweatheronline.com/feed/weather.ashx";
-	String apiKey="6421eb0ff1084035112211";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,21 +37,9 @@ public class AndroidVaja3Activity extends Activity {
 				// TODO Auto-generated method stub
 				TextView tw1 = (TextView)findViewById(R.id.queryText);
 				String query = (tw1).getText().toString();
-				String response = request(query, RestClient.Response.XML);
-				XMLParser parser = new XMLParser();
-				Vreme vreme = parser.parse(response);
-				
-				
-				if(vreme!=null){
-					StringBuilder rezultat = new StringBuilder();
-					rezultat.append("Iskalni niz: ");
-					rezultat.append(vreme.query+".\n");
-					rezultat.append("Trenutna temperatura je: " + vreme.temp + "°C");
-					
 
-					TextView tw2 = (TextView)findViewById(R.id.textView1);
-					tw2.setText(rezultat);
-				}
+				Download download = new Download();
+				download.execute(new String[]{query, "JSON"});
 				
 			}
 		});
@@ -58,30 +50,58 @@ public class AndroidVaja3Activity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				TextView tw1 = (TextView)findViewById(R.id.queryText);
-				String query = (tw1).getText().toString();
-				String response = request(query, RestClient.Response.JSON);
-				JsonParser parser = new JsonParser();
-				Vreme vreme = parser.parse(response);
+				String query = tw1.getText().toString();
+				Download download = new Download();
+				download.execute(new String[]{query, "JSON"});
 				
 				
-				if(vreme!=null){
-					StringBuilder rezultat = new StringBuilder();
-					rezultat.append("Iskalni niz: ");
-					rezultat.append(vreme.query+".\n");
-					rezultat.append("Trenutna temperatura je: " + vreme.temp + "°C");
-					
-
-					TextView tw2 = (TextView)findViewById(R.id.textView1);
-					tw2.setText(rezultat);
-				}
+				
 				
 			}
 		});
     }
     
+    public void SetTempValue(Vreme vreme){
+    	StringBuilder rezultat = new StringBuilder();
+		rezultat.append("Iskalni niz: ");
+		rezultat.append(vreme.query+".\n");
+		rezultat.append("Trenutna temperatura je: " + vreme.temp + "°C");
+		
+
+		TextView tw2 = (TextView)findViewById(R.id.textView1);
+		tw2.setText(rezultat);
+    }
     
     
-    String request(String query, RestClient.Response format){
+
+private class Download extends AsyncTask<String, Void, Vreme>{
+	String url ="http://free.worldweatheronline.com/feed/weather.ashx";
+	String apiKey="6421eb0ff1084035112211";
+	
+	
+	/* (non-Javadoc)
+	 * @see android.os.AsyncTask#doInBackground(Params[])
+	 */
+	@Override
+	protected Vreme doInBackground(String... params) {
+		String query = params[0];
+		RestClient.Response method = RestClient.Response.valueOf(params[1]);
+		Vreme vreme=null;
+		String response = request(query, method);
+		if(method==Response.JSON){
+		JsonParser parser = new JsonParser();
+		vreme = parser.parse(response);
+		}
+		else if(method==Response.XML){
+			XMLParser parser = new XMLParser();
+			vreme = parser.parse(response);
+		}
+		
+		
+		return vreme;
+	}
+	
+	private String request(String query, RestClient.Response format){
     	RestClient client = new RestClient(url);
     	client.AddParam("q", query);
     	client.AddParam("format", format.toString());
@@ -97,4 +117,13 @@ public class AndroidVaja3Activity extends Activity {
     	String response = client.getResponse();
     	return response;
     }
+	
+    protected void onPostExecute(Vreme vreme) {
+    	if(vreme!=null){
+		SetTempValue(vreme);
+		}
+    }
+	
 }
+}
+
